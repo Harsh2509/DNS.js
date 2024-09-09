667a8d01a2d97ef9da79e9938e07089af54449d4
harsh2509 <harshchauhan6176@gmail.com>
2024-09-09 19:59:31 +0530

    Initiating a UDP server

---
+++ app/index.ts
@@ -0,0 +1,18 @@
+import * as dgram from "dgram";
+
+console.log("Logs from your file will appear here!");
+
+const server = dgram.createSocket("udp4");
+server.bind(2053, "127.0.0.1");
+
+server.on("message", (msg: Buffer, remoteAddress: dgram.RemoteInfo) => {
+  try {
+    console.log(
+      `Message received from ${remoteAddress.address}:${remoteAddress.port}`
+    );
+    const response = Buffer.from("hello from server");
+    server.send(response, remoteAddress.port, remoteAddress.address);
+  } catch (e) {
+    console.log(`Error: ${e}`);
+  }
+});
