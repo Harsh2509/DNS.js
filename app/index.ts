03203ce2ae5351acee21e6fab9f721282b4ed0a3
harsh2509 <harshchauhan6176@gmail.com>
2024-09-10 15:06:54 +0530

    Send DNS header as response

--- app/index.ts
+++ app/index.ts
@@ -1,4 +1,23 @@
 import * as dgram from "dgram";
+import { DNSHeader, OPCODE, RCODE, type Header } from "./dns/header";
+
+// I am using the default headers for now.
+const defaultHeaders: Header = {
+  id: 12,
+  qr: 1, // For response, it is set   // For request, it is unset
+  opcode: OPCODE.STANDARD_QUERY,
+  aa: 0, // Authoritative answer
+  tc: 0, // Truncated: 1 if the message is larger than 512 bytes.
+  ra: 0, // Recursion available
+  rd: 0, // Recursion desired
+  z: 0, // Reserved for future use
+  rcode: RCODE.NO_ERROR,
+  qdcount: 0, // Number of questions
+  ancount: 0, // Number of answers
+  nscount: 0, // Number of authority records
+  arcount: 0, // Number of additional records
+};
+
 console.log("Logs from your file will appear here!");
 
 const server = dgram.createSocket("udp4");
@@ -9,7 +28,8 @@ server.on("message", (msg: Buffer, remoteAddress: dgram.RemoteInfo) => {
     console.log(
       `Message received from ${remoteAddress.address}:${remoteAddress.port}`
     );
-    const response = Buffer.from("hello from server");
+    const header = DNSHeader.encode(defaultHeaders);
+    const response = header;
     server.send(response, remoteAddress.port, remoteAddress.address);
   } catch (e) {
     console.log(`Error: ${e}`);
